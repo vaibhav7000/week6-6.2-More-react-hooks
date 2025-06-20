d8a52f0297138dc0468a7c659a2918084b30b00e
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-06-20 19:51:13 +0530

    adding more code to understand hooks

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,34 @@ import TodoWrapper from './Components/TodoWrapper';
 function App() {
     const [title, setTitle] = useState("");
     const [todos, setTodos] = useState([]);
+    const [preDefinedTodoName, setPreDefinedTodoName] = useState("");
+    const [priority, setPriority] = useState("preDefined");
+    const updatePreDefinedTitle = useCallback(function(value) {
+        setPreDefinedTodoName(value);
+    }, []);
 
     // I want to send the request to the backend-server when the title changes => using useEffect with title as dependency array
+
+    // This is easy but dangerous code
     useEffect(function() {
         fetchTodos();
-    }, [title])
+    }, [title, preDefinedTodoName]);
+
+    // useEffect does not allows to create async function direcly inside it because it either require a clean-up function or noting / undefined
 
     // using useCallback
     const fetchTodos = useCallback(async function() {
-        const finalTitle = title;
+        let finalTitle = undefined;
+        if(priority === "preDefined") {
+            finalTitle = preDefinedTodoName
+        } else {
+            finalTitle = title;
+        }
         const response = await fetch(`http://localhost:3000/todos/todoDetails?title=${finalTitle}`);
         const output = await response.json();
 
         setTodos(output.result);
-    }, [title]);
+    }, [title, preDefinedTodoName]);
 
 
     // this function gets only created at once when the component intially gets rendered / mounts on the screen, due to the useCallback it memorizes the reference and only changes when values present inside the depedency array changes
@@ -35,14 +49,48 @@ function App() {
         <div style={{
             display:"flex", flexDirection:"row", flexWrap:"wrap", gap: 20
         }}>
+            <PreDefinedTodoName updatePreDefinedTitle={updatePreDefinedTitle} />
+
             <TodoInput title={title} updateTitle={updateTitle} />
 
             <AllTodosByTitle todos={todos}/>
+
+            <button onClick={function() {
+                setPriority("preDefined")
+            }}>Priority PreDefined</button>
+
+            <button onClick={function() {
+                setPriority("input");
+            }}>Priority Input</button>
         </div>
     )
 }
 
 export default App;
+
+function PreDefinedTodoName({ updatePreDefinedTitle }) {
+
+    return (
+        <div style={{
+            display:"flex", flexDirection:"column", padding: 30, boxShadow:"0px 0px 10px 15px rgba(176, 176, 176, 0.55)", color:"black", borderRadius: 10, gap: 10, margin: 20
+        }}>
+
+            <button onClick={function() {
+                updatePreDefinedTitle("one")
+            }} >One</button>
+            <button onClick={function(){
+                updatePreDefinedTitle("two")
+            }}>Two</button>
+            <button onClick={function() {
+                updatePreDefinedTitle("three")
+            }}>Three</button>
+            <button onClick={function(){
+                updatePreDefinedTitle("four")
+            }} >Four</button>
+
+        </div>
+    )
+}
 
 function AllTodosByTitle({ todos }) {
 
